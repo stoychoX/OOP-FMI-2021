84cd6dff3d7807f01e5764f4fb349a4d1df783a4
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2021-05-07 03:57:15 +0300

    Removed psn

--- tests/queue-kPriorityqueue-tests.cpp
+++ tests/queue-kPriorityqueue-tests.cpp
@@ -1,6 +1,6 @@
 //
 //A .cpp file containing tests
-//of psn::Queue and psn::kPriorityQueue
+//of Queue and kPriorityQueue
 //
 #include<iostream>
 #include"Queue.hpp"
@@ -9,49 +9,34 @@
 
 int main() {
 	std::cout << "==========QUEUE==========\n";
-  
 	std::cout << "Creating empty queue with int values q1...\n" << end;
-	psn::Queue<int> q1;
+	Queue<int> q1;
 
 	std::cout << "Adding numbers from 1 to 10 in..." << end;
-  
 	for (int i = 0; i < 10; i++)
 		q1.enqueue(i + 1);
-    
 	std::cout << "Front: " << q1.c_front() << " back: " << q1.c_back() << end;
-  
 	std::cout << "Popping two elements from q1..." << end;
-  
 	q1.dequeue();
 	q1.dequeue();
-  
 	std::cout << "Front: " << q1.c_front() << " back: " << q1.c_back() << end;
-  
 	std::cout << "Creating qCpy, copy of q1: " << end;
-  
-	psn::Queue<int> qCpy(q1);
-  
+	Queue<int> qCpy(q1);
 	std::cout << "Trying == on q1 and qCpy: " << (q1 == qCpy) << end;
-  
 	std::cout << "Popping from qCpy and trying == again: ";
 	qCpy.dequeue();
 	std::cout << (q1 == qCpy) << end;
 
 	std::cout << "==========KPRIORITYQUEUE==========\n";
-  
 	std::cout << "Creating kPriorityQueue containing elements of type int k1, having 3 priorities..." << end;
-  
-	psn::kPriorityQueue<int> k1(3);
-  
+	kPriorityQueue<int> k1(3);
 	std::cout << "Adding the numbers from 1 to 10 with priority ind % 3\n" << end;
 	for (int i = 0; i < 10; i++)
 		k1.enqueue(i + 1, (i + 1) % 3);
 
 	std::cout << "Top: " << k1.c_top() << end;
-  
 	std::cout << "Popping each element:\n";
 	for (int i = 0; i < 10; i++)
 		std::cout << k1.dequeue()<<" ";
-    
-	std::cout << "\nFirst popping elements with priority 2, which are equivalent to numbers with mod 2, then with priority 1, and last popping the elements divisible by 3." << end;
+	std::cout << "\nFirst popping elements with mod 2, then with mod 1 and last popped are the elements divisible by 3" << end;
 }
