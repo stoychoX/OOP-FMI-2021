2b5809434d3acccbdcdc4bde89930103f4656654
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2022-05-11 17:34:29 +0300

    Update pair.hpp

--- classes/pair.hpp
+++ classes/pair.hpp
@@ -8,60 +8,33 @@ public:
 	T first;
 	U second;
 
-	//construct:
 	pair();
 	pair(const T& _first, const U& _second);
-	pair(const pair<T, U>& other);
 
-	//operator=
-	pair& operator=(const pair<T, U> & other);
 	//helpers:
 	pair make_pair(const T& _valFirst, const U& _valSecond) const;
 
-	const T c_first() const;
-	const U c_second() const;
-private:
-	void copy(const pair<T, U>& other);
+	const T& c_first() const;
+	const U& c_second() const;
 };
 
 template<class T, class U>
-inline void pair<T, U>::copy(const pair<T, U>& other){
-	this->first = other.first;
-	this->second = other.second;
-}
+pair<T, U>::pair() : first(T()), second(U()){}
 
 template<class T, class U>
-inline pair<T, U>::pair() : first(T()), second(U()){}
+pair<T, U>::pair(const T& _first, const U& _second) : first(_first), second(_second) {}
 
 template<class T, class U>
-inline pair<T, U>::pair(const T& _first, const U& _second) : first(_first), second(_second) {}
-
-template<class T, class U>
-inline pair<T, U>::pair(const pair<T, U>& other){
-	copy(other);
-}
-
-template<class T, class U>
-inline pair<T, U>& pair<T, U>::operator=(const pair<T, U>& other)
-{
-	if (this != &other)
-		copy(other);
-
-	return *this;
-}
-
-template<class T, class U>
-inline pair<T, U> pair<T, U>::make_pair(const T& _valFirst, const U& _valSecond) const{
+pair<T, U> pair<T, U>::make_pair(const T& _valFirst, const U& _valSecond) const{
 	return pair(_valFirst, _valSecond);
 }
 
 template<class T, class U>
-inline const T pair<T, U>::c_first() const{
+const T& pair<T, U>::c_first() const{
 	return this->first;
 }
 
 template<class T, class U>
-inline const U pair<T, U>::c_second() const
-{
+const U& pair<T, U>::c_second() const {
 	return this->second;
 }
