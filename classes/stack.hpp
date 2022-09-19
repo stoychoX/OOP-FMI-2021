9c8b051d8529f8a8a68b3e77e3cf8838df6beec1
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2022-09-20 02:37:38 +0300

    Update stack impl + add tests

--- classes/stack.hpp
+++ classes/stack.hpp
@@ -1,164 +1,121 @@
 /*****************************
-Stack implementation.
+Static stack implementation.
 18.09.2021
+
+quick update: 20.09.2022
 ******************************/
 
-#ifndef STACK_HPP
-#define STACK_HPP
+#ifndef STATIC_STACK_HPP
+#define STATIC_STACK_HPP
+
 #include<exception>
+#include<cassert>
 
-#define MAXSIZE 100
-
-template<class T>
+template<class T, unsigned stackSize = 100>
 class stack {
 private:
-	int tos;
-	T* data;
-	int size_of_stack;
-
+	T data[stackSize];
+	size_t tos;
 public:
-	//construct
 	stack();
-	stack(const stack<T>& other);
-	stack(stack<T>&& other);
-
-	//operator= 
-	stack& operator=(const stack& other);
-	stack& operator=(stack&& other);
 
 	void push(const T& arg);
-	T& pop();
+	const T& pop();
 
-	int size() const; 
-	void clear();
+	size_t size() const;
+	size_t capacity() const;
 
 	const T& top() const;
 
-	//indicators:
 	bool isFull() const;
 	bool isEmpty() const;
-
-	//destruct:
-	~stack();
-private:
-	//helpers:
-	void copy(const stack<T>& other);
-	void free();
 };
 
 #endif // !STACK_HPP
 
-template<class T>
-inline stack<T>::stack() : size_of_stack(MAXSIZE), tos(0) {
-	this->data = new T[size_of_stack];
-}
+template<class T, unsigned stackSize>
+inline stack<T, stackSize>::stack() : tos{ 0 }, data{} {}
 
-//copy constructor:
-template<class T>
-inline stack<T>::stack(const stack<T>& other) {
-	copy(other);
-}
-
-template<class T>
-inline stack<T>::stack(stack<T>&& other) {
-	this->data = other.data;
-	this->size_of_stack = other.size_of_stack;
-	this->tos = other.tos;
-
-	other.data = nullptr;
-	other.tos = other.size_of_stack = 0;
-}
-
-template<class T>
-inline stack<T>& stack<T>::operator=(const stack<T>& other) {
-	if (this != &other) {
-		free();
-		copy(other);
-	}
-	return *this;
-}
-
-template<class T>
-inline stack<T>& stack<T>::operator=(stack<T>&& other) {
-	this->data = other.data;
-	this->tos = other.tos;
-	this->size_of_stack = other.size_of_stack;
-
-	other.data = nullptr;
-	other.size_of_stack = other.tos = 0;
-
-	return *this;
-}
-
-template<class T>
-inline void stack<T>::push(const T& arg) {
+template<class T, unsigned stackSize>
+inline void stack<T, stackSize>::push(const T& arg) {
 	if (isFull())
-		throw std::domain_error("Stack is full");
-	else
-		this->data[tos++] = arg;
+		throw std::exception("Stack is full");
+
+	this->data[tos++] = arg;
 }
 
-template<class T>
-inline bool stack<T>::isFull() const {
-	return (this->tos == this->size_of_stack - 1);
+template<class T, unsigned stackSize>
+inline bool stack<T, stackSize>::isFull() const {
+	return tos == stackSize;
 }
 
-template<class T>
-inline bool stack<T>::isEmpty() const {
-	return (this->tos == 0);
+template<class T, unsigned stackSize>
+inline bool stack<T, stackSize>::isEmpty() const {
+	return tos == 0;
 }
 
-template<class T>
-inline stack<T>::~stack() {
-	free();
-}
-
-template<class T>
-inline void stack<T>::copy(const stack<T>& other) {
-	this->data = new T[other.size_of_stack];
-	this->size_of_stack = other.size_of_stack;
-	this->tos = other.tos;
-
-	try {
-		for (int i = 0; i < other.tos; i++)
-			this->data[i] = other.data[i];
-	}
-	catch (...) {
-		clear();
-		throw;
-	}
-}
-
-template<class T>
-inline void stack<T>::free() {
-	delete[] this->data;
-}
-
-template<class T>
-T& stack<T>::pop() {
-	if (this->isEmpty())
-		throw std::domain_error("stack is empty");
-
-	return this->data[--tos];
-}
-
-template<class T>
-int stack<T>::size() const {
-	return this->tos;
-}
-
-template<class T>
-inline void stack<T>::clear() {
-	free();
-
-	this->data = new T[this->size_of_stack];
-	tos = 0;
-}
-
-template<class T>
-inline const T& stack<T>::top() const {
+template<class T, unsigned stackSize>
+const T& stack<T, stackSize>::pop() {
 	if (isEmpty())
-		throw std::domain_error("Empty stack");
+		throw std::exception("Stack is empty");
 
-	return this->data[tos - 1];
+	return data[--tos];
+}
+
+template<class T, unsigned stackSize>
+size_t stack<T, stackSize>::size() const {
+	return tos;
+}
+
+template<class T, unsigned stackSize>
+size_t stack<T, stackSize>::capacity() const {
+	return stackSize;
+}
+
+template<class T, unsigned stackSize>
+inline const T& stack<T, stackSize>::top() const {
+	if (isEmpty())
+		throw std::exception("Empty stack");
+
+	return data[tos - 1];
+}
+
+void runTests() {
+	stack<int> s;
+
+	for (size_t i = 0; i < s.capacity(); i++)
+		s.push(i);
+
+	assert(s.isFull());
+
+	try { s.push(0); assert(false); }
+	catch (...) {}
+
+	for (size_t i = 0; i < s.capacity(); i++) {
+		int curr = s.top();
+		s.pop();
+		assert(curr == s.capacity() - i - 1);
+	}
+
+	assert(s.isEmpty());
+
+	if (s.capacity() < 10)
+		return;
+
+	for (size_t i = 0; i < 10; i++)
+		s.push(i);
+
+	assert(s.size() == 10);
+	
+	while (!s.isEmpty()) { s.pop(); }
+
+	try { s.pop(); assert(false); }
+	catch (...) {}
+
+	try { s.top(); assert(false); }
+	catch (...) {}
+}
+
+int main() {
+	runTests();
 }
